68612a634fa62ab5d6b7e7ca5a499613a200f08b
Poppy Wu <poppywu@Poppys-MacBook-Air.local>
2024-03-01 14:28:54 -0800

    add useMemo and useCallback hooks

--- components/List.tsx
+++ components/List.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo,useCallback } from "react";
 import ListItem from "./ListItem";
 import { useRouter } from "next/navigation";
 import { EpisodeType } from "@/app/episodes/[id]/page";
-import styles from '@/styles/List.module.css';
+import styles from "@/styles/List.module.css";
 
 export interface EpisodeObj {
   name: string;
@@ -14,47 +14,78 @@ function List() {
   const router = useRouter();
   const [focus, setFocus] = useState(-1);
   const [episodes, setEpisodes] = useState<EpisodeType[]>([]);
-  const [page,setPage]=useState(1);
-  const handleClick = (id: number) => {
+  const [page, setPage] = useState(1);
+  const [input, setInput] = useState("");
+  const [term, setTerm] = useState("");
+  const handleClick = useCallback((id: number) => {
     return () => {
-      if(focus!==id){
+      if (focus !== id) {
         setFocus(id);
-      router.push(`/episodes/${id}`);
-      }else{
+        router.push(`/episodes/${id}`);
+      } else {
         setFocus(-1);
-        router.push('/');
+        router.push("/");
       }
     };
-  };
-  const fetchMoreEpisodes = async (page:number) => {
+  },[episodes]);
+  const fetchMoreEpisodes = async (page: number) => {
     try {
       let response = await fetch(
         `https://rickandmortyapi.com/api/episode?page=${page}`
       );
       let data = await response.json();
-      setEpisodes([...episodes,...data?.results]);
-      if(data?.info.next!==null){
-        setPage(page+1);
+      setEpisodes([...episodes, ...data?.results]);
+      if (data?.info.next !== null) {
+        setPage(page + 1);
       }
     } catch (error) {
       console.error(error);
     }
   };
-  useEffect(()=>{
+  useEffect(() => {
     fetchMoreEpisodes(page);
-  },[page])
+  }, [page]);
+  const handleChange = (e: any) => {
+    setInput(e.target.value);
+  };
+  const filterEpisode = useMemo(
+    () =>
+      episodes.filter((episode) =>
+        episode.name.toLowerCase().includes(term.toLocaleLowerCase())
+      ),
+    [term, episodes]
+  );
+  const handleSearch = () => {
+    setTerm(input);
+  };
+  const handleReset = () => {
+    setInput('');
+    setTerm('');
+  };
   return (
     <div className={styles.lists}>
       <h1>EPISODES</h1>
-      {episodes?.map((episodeObj) => (
-        <ListItem
-          title={episodeObj.name}
-          key={episodeObj.id}
-          isFocused={focus == episodeObj.id}
-          onClick={handleClick(episodeObj.id)}
-        />
-      ))}
-      </div>
+      <input type="text" value={input} onChange={handleChange} placeholder="Search episodes..."/>
+      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleReset}>Reset</button>
+      {term.length
+        ? filterEpisode?.map((episodeObj) => (
+            <ListItem
+              title={episodeObj.name}
+              key={episodeObj.id}
+              isFocused={focus == episodeObj.id}
+              onClick={handleClick(episodeObj.id)}
+            />
+          ))
+        : episodes?.map((episodeObj) => (
+            <ListItem
+              title={episodeObj.name}
+              key={episodeObj.id}
+              isFocused={focus == episodeObj.id}
+              onClick={handleClick(episodeObj.id)}
+            />
+          ))}
+    </div>
   );
 }
 
